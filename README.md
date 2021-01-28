28612966dd86258bbb3ccfd8bbe13367da8fcd58
MHindermann <maximilian.hindermann@unibas.ch>
2021-01-28 15:15:12 +0100

    added histogram for keywords

--- README.md
+++ README.md
@@ -21,9 +21,19 @@ Refined by: ORGANIZATIONS-ENHANCED: ( UNIVERSITY OF BASEL ) `, 5625 hits
 4. Run "find duplicates", remove 5803 records. But a quick manual check reveals that many duplicates have been missed.
 5. Manual deduplication removes an additional 1720 records; the remaining 10115 records are saved as `refined/deduplicated.enlx` resp. `refined/deduplicated.xml`.
 6. The Endnote XML export format is unwieldy; more importantly, it is not correctly parsed by OpenRefine. We can amend this by running `_Utility.xml2json(DIR + "/refined/deduplicated.xml", DIR + "/refined/deduplicated.json")`; the output is saved as `refined/deduplicated.json`.
+7. Declutter `deduplicated.json` in OpenRefine (remove empty columns, adapt column names, etc.); export data in decade-slices as `.csv` to `refined/20210128` and convert to `.json`.
 
 ### Word cloud based on keywords
 1. Due to the deduplication process different kinds of keywords can no longer be distinguished. 
-2. We can get an approximation of a word cloud by means of a histogram. Since the given keywords are messy (different ways of spelling one term, capitalization, etc.) we need to clean them first. 
+2. We can get an approximation of a word cloud by means of a histogram. Since the given keywords are messy (different ways of spelling one term, capitalization, etc.) we need to clean them first. To create a histogram for arbitrary slices of records (here the slices by decade saved in `refined/20210128`) do this:
+   ```
+   _Keywords.slices2histogram(DIR + "/refined/20210128/deduplicated_1971-1981.json",
+                           DIR + "/refined/20210128/deduplicated_1981-1991.json",
+                           DIR + "/refined/20210128/deduplicated_1991-2001.json",
+                           DIR + "/refined/20210128/deduplicated_2001-2011.json",
+                           DIR + "/refined/20210128/deduplicated_2011-2021.json",
+                           save_path=DIR + "/refined/20210128/histogram_1971-2021.json")
+   ```
+Not yet implemented:
 3. We might also want to unify the singular and plural of one and the same keyword.
 4. We might also want to map each keyword to a keyword from a controlled vocabulary in order to prevent semantic duplications. However, this is more involved due to the prevalence of compound keywords such as "Mechanistic Target of Rapamycin Complex 2/genetics/*metabolism" which require some more care. So this step is skipped for now.
--- scripts.py
+++ scripts.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
 from typing import List, Optional, Dict, Union, Tuple
 from json import load, dump
-from datetime import datetime
-from annif_client import AnnifClient
 import os.path
-import urllib3
 import xmltodict
 
 
@@ -26,6 +23,19 @@ class _Utility:
 
         with open(file_path, encoding="utf-8") as file:
             loaded = xmltodict.parse(file.read().replace('<style face="normal" font="default" size="100%">', "").replace('</style>', ""))
+
+            return loaded
+
+    @classmethod
+    def load_json(cls,
+                  file_path: str) -> list:
+        """ Load a JSON object from file.
+
+        :param file_path: complete path to file including filename and extension
+        """
+
+        with open(file_path, encoding="utf-8") as file:
+            loaded = load(file)
 
             return loaded
 
@@ -54,3 +64,83 @@ class _Utility:
         file = cls.load_xml(file_path)
 
         cls.save_json(file, save_path)
+
+
+class _Keywords:
+    """ A collection of functions for manipulating keywords. """
+
+    @classmethod
+    def extract_keywords(cls,
+                         *file_paths: str) -> List[str]:
+        """ Extract keywords per item from JSON file.
+
+        :param file_paths: complete paths to file including filename and extension
+        """
+
+        keywords = []
+        for file_path in file_paths:
+            data = _Utility.load_json(file_path)
+            print(f"Working on {file_path}...", end=" ")
+            for item in data:
+                keywords.append(item.get("keywords"))
+            print(" done.")
+
+        return keywords
+
+    @classmethod
+    def clean_keyword(cls,
+                      keyword: str) -> str:
+        """ Clean a keyword.
+
+        :param keyword: the keyword
+        """
+
+        # make lower case:
+        keyword = keyword.lower()
+        # remove *:
+        keyword = keyword.replace("*", "")
+        # remove whitespace:
+        keyword = keyword.strip()
+
+        return keyword
+
+    @classmethod
+    def make_histogram(cls,
+                       keywords: List[str]) -> List[Dict]:
+        """ Make a histogram for keywords.
+
+        :param keywords: the keywords
+        """
+        histogram = dict()
+        for keyword in keywords:
+            if keyword in histogram:
+                histogram[keyword] = histogram[keyword] + 1
+            else:
+                histogram[keyword] = 1
+
+        openrefine_histogram = []
+        for entry in histogram:
+            openrefine_histogram.append({"keyword": entry, "occurrences": histogram.get(entry)})
+
+        return openrefine_histogram
+
+    @classmethod
+    def slices2histogram(cls,
+                         *file_paths: str,
+                         save_path: str = DIR + "/refined/histogram.json"):
+        """ Turn slices of records into histogram based on keywords.
+
+        :param file_paths: complete paths to file including filename and extension
+        :param save_path: complete path to save folder including filename without extension
+        """
+
+        keywords = _Keywords.extract_keywords(*file_paths)
+        clean_keywords = []
+        for keyword in keywords:
+            clean_keywords.append(_Keywords.clean_keyword(keyword))
+        histogram = _Keywords.make_histogram(clean_keywords)
+        _Utility.save_json(histogram, save_path)
+
+
+_Keywords.slices2histogram(DIR + "/refined/20210128/deduplicated_2011-2021.json",
+                           save_path=DIR + "/refined/20210128/histogram_2011-2021.json")
